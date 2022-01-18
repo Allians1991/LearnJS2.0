78568f093779a40cb86fc05e7c051aad448cf392
Anton <allians19@gmail.com>
2022-01-18 09:32:16 +0500

    Поработали с циклами, автоматизировали наше мини приложение

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@
 
 const numbersOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 		
-
-
-
-
 const personalMovieDB = {
 	count: numbersOfFilms,
 	movies: {},
@@ -14,17 +10,69 @@ const personalMovieDB = {
 	privat: false
 };
 
+// for(let i = 0; i < 2; i++) {
+// 	const	a = prompt('Один из послдедних просмотренных фильмов?', ''),
+// 			b = +prompt('на сколько оценити его?', '');
+// 	if(a != null && b != null && a != '' && b != '' && a.length < 50 && b <= 10) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('Done');
+// 	}else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	personalMovieDB.movies[a] = b;
+// }
 
-const a = prompt('Один из послдедних просмотренных фильмов?', ''),
-		b = +prompt('на сколько оценити его?', ''),
-		c = prompt('Один из послдедних просмотренных фильмов?', ''),
-		d = +prompt('на сколько оценити его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let i = 1;
+
+//Цикл while
+// while(i <= 2) {
+// 	const	a = prompt('Один из послдедних просмотренных фильмов?', ''),
+// 			b = +prompt('на сколько оценити его?', '');
+// 	if(a != null && b != null && a != '' && b != '' && a.length < 50 && b <= 10) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('Done');
+// 	}else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	i++;
+// 	personalMovieDB.movies[a] = b;
+// }
+
+//Цикл do while
+// do {
+// 	const	a = prompt('Один из послдедних просмотренных фильмов?', ''),
+// 			b = +prompt('на сколько оценити его?', '');
+// 	if(a != null && b != null && a != '' && b != '' && a.length < 50 && b <= 10) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('Done');
+// 	}else {
+// 		console.log('error');
+// 		i--;
+// 	}
+// 	i++;
+// 	personalMovieDB.movies[a] = b;
+// }
+// while(i <= 2);
+
+
+if(personalMovieDB.count < 10 && personalMovieDB.count != null) {
+	alert('Просмотренно довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	alert('Вы классический зритель');
+} else if(personalMovieDB.count >= 30) {
+	alert('Вы киноман');
+} else {
+	alert('Что то пошло не так :-(');
+}
+
+
 
 
 
 
 
 console.log(personalMovieDB);
+
