86dedb4d64106d0ff062c0ad110d570d324ef798
Allians <allians19@yandex.ru>
2022-03-10 09:15:30 +0500

    JS в работе: урок 1(создали табы)

--- js/app.js
+++ js/app.js
@@ -1,73 +1,42 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', ()=> {
 
-const personalMovieDB = {
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: function () {
-		personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-		while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-			personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-		}
-	},
-	rememberMyFilms: function () {
-		for(let i = 0; i < 2; i++) {
-			const	a = prompt('Один из послдедних просмотренных фильмов?', ''),
-					b = +prompt('на сколько оценити его?', '');
-			if(a != null && b != null && a != '' && b != '' && a.length < 50 && b <= 10) {
-				personalMovieDB.movies[a] = b;
-				console.log('Done');
-			}else {
-				console.log('error');
-				i--;
-			}
-			personalMovieDB.movies[a] = b;
-		}
-	},
-	
-	writeYourGenres: function () {
-		for(let i = 1; i < 2; i++) {
 
-			let genres = prompt(`Введите Ваш любимый жанр через запятую`).toLowerCase();
-
-			if (genres === '' || genres == null) {
-				console.log('Неверно');
-				i--;
-			} else {
-				personalMovieDB.genres = genres.split(', ');
-				personalMovieDB.genres.sort();
-			}
-		}
-		personalMovieDB.genres.forEach((item, i) => {
-			console.log(`Любимый жанр ${i + 1} - это ${item}`);
+	const tabsParent = document.querySelector('.tabheader__items'),
+			tabs = tabsParent.querySelectorAll('.tabheader__item'),
+			tabsContent = document.querySelectorAll('.tabcontent');
+			
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
 		});
-	},
-	detectPersonalLevel: function () {
-		if(personalMovieDB.count < 10 && personalMovieDB.count != null) {
-			alert('Просмотренно довольно мало фильмов');
-		} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-			alert('Вы классический зритель');
-		} else if(personalMovieDB.count >= 30) {
-			alert('Вы киноман');
-		} else {
-			alert('Что то пошло не так :-(');
-		}
-	},
-	showMyDB: function(hidden) {
-		if(!hidden){
-			console.log(personalMovieDB);
-		}
-	},
-	toggleVisibleMyDB: function () {
-		if (personalMovieDB.privat) {
-			personalMovieDB.privat = false;
-		} else {
-			personalMovieDB.privat = true;
-		}
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active');
+		});
 	}
-};
 
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
 
+	hideTabContent();
+	showTabContent();
 
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if(target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) =>{
+				if(item == target) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+
+	});
+
+});
